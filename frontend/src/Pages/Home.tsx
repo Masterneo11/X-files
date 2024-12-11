467b61fcd86685909917a9d60a37e6b0e685a1ef
Mathew Zamacona <mateoszamacona@icloud.com>
2024-12-11 09:58:46 -0700

    url edit for frontend

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -5,7 +5,7 @@ import PodiumIcon from "../assets/podium-victory-leader.svg";
 import Modal from "../Components/Modal";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE_URL = "https:backend:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 interface Event {
     id: string;
